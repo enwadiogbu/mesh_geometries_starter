70577789f38a4f15156e58a75f62cf88e8969e07
enwadiogbu <emekanwadiogbu.jr@me.com>
2025-06-29 01:28:08 -0700

    practicing with the provided geometries given in ThreeJS.org.

--- src/script.js
+++ src/script.js
@@ -6,22 +6,28 @@ const scene = new THREE.Scene();
 
 // create custom geometry
 
-const vertices = new Float32Array([
-  0,0,0,
-  0,2,0,
-  2,0,0
-]);
+// const vertices = new Float32Array([
+//   0,0,0,
+//   0,2,0,
+//   2,0,0
+// ]);
 
 // pass the vertices array to a BufferAttribute threejs.org
-const bufferAttribute = new THREE.BufferAttribute(vertices, 3);
-const geometry = new THREE.BufferGeometry();
-geometry.setAttribute('position', bufferAttribute);
+// const bufferAttribute = new THREE.BufferAttribute(vertices, 3);
+// const geometry = new THREE.BufferGeometry();
+// geometry.setAttribute('position', bufferAttribute);
 // add objects to the scene
 // const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "dodgerblue", wireframe: true });
-const cubeMesh = new THREE.Mesh(geometry, cubeMaterial);
 
-scene.add(cubeMesh);
+// add objects to a scene
+
+const geometry = new THREE.CapsuleGeometry(5,3,16,52,24);
+const material = new THREE.MeshBasicMaterial({color: 'dodgerblue', wireframe: true});
+const capsule = new THREE.Mesh(geometry, material);
+// const cubeMaterial = new THREE.MeshBasicMaterial({ color: "dodgerblue", wireframe: true });
+// const cubeMesh = new THREE.Mesh(geometry, cubeMaterial);
+
+scene.add(capsule);
 
 
 
